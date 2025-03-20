39227e1d1da7b85eb51f1e0cd9668bd269a6c281
patel-param-09 <patelparam9304@gmail.com>
2025-03-20 13:02:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./app.scss";
+import NavBar from "./components/NavBar/NavBar";
+
+const App = () => {
+  return (
+    <div>
+      <section>
+        <NavBar />
+      </section>
+      <section>Parallax</section>
+      <section>Skills</section>
+      <section>Parallax</section>
+      <section>Projects</section>
+      <section>Parallax</section>
+      <section>About</section>
+      <section>Projects</section>
+      <section>Parallax</section>
+      <section>Contact</section>
+    </div>
+  );
+};
+
+export default App;
