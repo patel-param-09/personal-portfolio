37aa0b31a5699101a04fce8d73f8bf1ac5d3025d
patel-param-09 <patelparam9304@gmail.com>
2025-03-21 17:13:23 +0530

    Create Project Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,40 @@ import "./app.scss";
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./components/Home/Home";
 import Parallax from "./components/Parallax/Parallax";
+import Skills from "./components/Skills/Skills";
+import Project from "./components/Projects/Project";
 
 const App = () => {
   return (
     <div>
+      {/* Home Page */}
       <section id="Home">
         <NavBar />
         <Home />
       </section>
+      {/* Parallax Page */}
       <section id="Skills">
         <Parallax type="skills" />
       </section>
-      <section>Skills</section>
+      {/* Skills Page */}
+      <section>
+        <Skills />
+      </section>
+      {/* Parallax Page */}
       <section id="Projects">
         <Parallax type="projects" />
       </section>
-      <section>Projects</section>
-      <section id="About">Parallax</section>
+      {/* Projects Page */}
+      <Project />
+      {/* Parallax Page */}
+      <section id="About">
+        <Parallax type="about" />
+      </section>
+      {/* About Page */}
       <section>About</section>
+      {/* Parallax Page */}
       <section id="Contact">Parallax</section>
+      {/* Contact Page */}
       <section>Contact</section>
     </div>
   );
